8147179a3b270e274815fbe5534e60ff560ab179
GRusla-n <72308587+GRusla-n@users.noreply.github.com>
2023-02-22 19:26:20 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,6 +17,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
+            Try cd
             Get started by editing&nbsp;
             <code className={styles.code}>src/pages/index.tsx</code>
           </p>
